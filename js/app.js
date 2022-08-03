d5564f81b5b2fb6d00a3510a3e1eb26c908b0a68
Sansone11 <sansonemirco@gmail.com>
2022-08-03 16:20:39 +0200

    milestone1-edit

---
+++ js/app.js
@@ -0,0 +1,59 @@
+console.log('JS ok');
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1:
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+// nome           role                    image
+// Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	            scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
+
+// PSEUDOCODICE
+
+
+// crea una funzione che genera membri del team i cui valori sono nome,ruolo ,immagine
+// crea un array di oggetti con le informazioni fornite
+// cicla gli elementi per ogni membro
+// stampa in console con singoli attributi cono dot.notation
+// crea elemento div 
+// aggiungere(+=) all'inner del container il div
+// stampa il contenuto delle stringhe dentro il div con Inner.
+
+
+
+// crea una funzione che genera membri del team i cui valori sono nome,ruolo ,immagine
+function createMember(nome, ruolo, image) {
+    const member = {
+        nome: nome,
+        ruolo: ruolo,
+        image: image,
+    }
+    return member
+    
+    
+}
+// crea un array di oggetti con le informazioni fornite
+const membri = [
+    createMember('wayne-barnett', 'founder-ceo', 'wayne-barnett-founder-ceo.jpg'),
+    createMember('angela-caroll', 'chief-editor', 'angela-caroll-chief-editor.jpg'),
+    createMember('Walter Gordon', 'Office Manager', ' walter-gordon-office-manager.jpg'),
+    createMember('Angela Lopez', 'Social Media Manager', 'angela-lopez-social-media-manager.jpg'),
+    createMember('Scott Estrada', 'Developer', 'scott-estrada-developer.jpg'),
+    createMember('Barbara Ramos', 'Graphic Designer', 'barbara-ramos-graphic-designer.jpg'),
+]
+// console.log(membri)
+// cicla gli elementi per ogni membro
+membri.forEach(element => {
+    // stampa in console
+    console.log(element.nome,element.ruolo,element.image);
+    
+})
+
+
